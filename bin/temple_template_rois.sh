5ecfa5e22b52fae641d7c267abc5a65f85c4c3f4
owenfriend24 <owenwfriend@gmail.com>
2023-06-13 13:01:47 -0500

    Create temple_template_rois.sh

---
+++ bin/temple_template_rois.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# Transform template ROIs to native resolution template space.
+
+if [[ $# -lt 4 ]]; then
+    echo "Usage: temple_template_rois.sh templatedir space outdir subject"
+    exit 1
+fi
+
+templatedir=$1
+space=$2
+outdir=$3
+subject=$4
+
+subjdir=${outdir}/sub-${subject}
+
+reference_name=task-struct_run-1_space-${space}_desc-brain_mask
+
+reference_file=${subjdir}/func/sub-${subject}_${reference_name}.nii.gz
+
+# brain mask
+name=desc-brain_mask
+template_file=${templatedir}/tpl-${space}/tpl-${space}_res-01_${name}.nii.gz
+if [[ ! -f ${template_file} ]]; then
+    echo "Template file not found: ${template_file}"
+    exit 1
+fi
+output_file=${subjdir}/anat/sub-${subject}_space-${space}_${name}.nii.gz
+antsApplyTransforms -d 3 -e 0 \
+    -i "${template_file}" \
+    -r "${reference_file}" \
+    -o "${output_file}" \
+    -n NearestNeighbor
+
+# probabilistic labels
+for name in label-{brain,GM,WM,CSF}_probseg; do
+    template_file=${templatedir}/tpl-${space}/tpl-${space}_res-01_${name}.nii.gz
+    if [[ ! -f ${template_file} ]]; then
+        echo "Template file not found: ${template_file}"
+        exit 1
+    fi
+    output_file=${subjdir}/anat/sub-${subject}_space-${space}_${name}.nii.gz
+    antsApplyTransforms -d 3 -e 0 \
+        -i "${template_file}" \
+        -r "${reference_file}" \
+        -o "${output_file}" \
+        -n Linear
+done
