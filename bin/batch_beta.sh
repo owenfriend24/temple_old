61f70e510a364158352252689b0c8280dc94717a
owenfriend24 <owenwfriend@gmail.com>
2024-02-01 17:37:11 -0600

    Update batch_beta.sh

--- bin/batch_beta.sh
+++ bin/batch_beta.sh
@@ -16,7 +16,7 @@ source /home1/09123/ofriend/analysis/temple/profile
 
 conf=csf:csf_derivative1:white_matter:white_matter_derivative1:trans_x:trans_x_derivative1:trans_y:trans_y_derivative1:trans_z:trans_z_derivative1:rot_x:rot_x_derivative1:rot_y:rot_y_derivative1:rot_z:rot_z_derivative1
 
-for run in 2 3 4 5 6; do
+for run in 1 2 3 4 5 6; do
     betaseries-bids --confound-measures ${conf} ${fm_dir} ${fm_dir} '${fm_dir}/beta' ${subject} arrow 0${run} T1w \
     gm_func_dilated ${fs_dir}/sub-${subject}/mri/out/brainmask_func_dilated.nii.gz object
 
