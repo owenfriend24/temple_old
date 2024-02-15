cde59a4764e1788141e0944f82593d22981dc12d
owenfriend24 <owenwfriend@gmail.com>
2024-02-15 13:03:34 -0600

    Update batch_beta.sh

--- bin/batch_beta.sh
+++ bin/batch_beta.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 if [[ $# -lt 3 ]]; then
-    echo "Usage: batch_beta.sh fm_dir fs_dir out_dir subject"
+    echo "Usage: batch_beta.sh fm_dir fs_dir subject"
     exit 1
 fi
 
 fm_dir=$1
 fs_dir=$2
-out_dir=$3
-subject=$4
-
+subject=$3
 
 # Activate your virtual environment if you're using one
 source /home1/09123/ofriend/analysis/temple/profile
+
+mkdir /corral-repl/utexas/prestonlab/temple/beta/sub-${subject}
+
+out_dir=/corral-repl/utexas/prestonlab/temple/beta/sub-${subject}
 
 conf=csf:csf_derivative1:white_matter:white_matter_derivative1:trans_x:trans_x_derivative1:trans_y:trans_y_derivative1:trans_z:trans_z_derivative1:rot_x:rot_x_derivative1:rot_y:rot_y_derivative1:rot_z:rot_z_derivative1
 
@@ -22,3 +24,4 @@ for run in 1 2 3 4 5 6; do
 
     echo "Created beta image for run ${run}!"
 done
+
