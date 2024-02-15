d29aa2621147b677d84d10e649f22a0841165682
owenfriend24 <owenwfriend@gmail.com>
2024-02-15 14:17:43 -0600

    Update fix_arrow.py

--- bin/fix_arrow.py
+++ bin/fix_arrow.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
-#
-# fix collector runs to correctly reflect triads and objects
+# 
+# fix arrow runs to correctly reflect triads and objects
 
 from pathlib import Path
 import pandas as pd
 import os
 import argparse
 
+def run(command):
+    #print(f"Running command: {command}")
+    subprocess.run(command, shell=True)
+
 def main(data_dir, sub):
+    
+
+    
     func_dir = data_dir + f'/sub-{sub}/func/'
+
+
+    run('mkdir ${func_dir}/orig_events')
+    run('cp ${func_dir}/*events.tsv ${func_dir/orig_events/')
+    
     a1 = pd.read_table(func_dir + f'sub-{sub}_task-arrow_run-01_events.tsv')
     a2 = pd.read_table(func_dir + f'sub-{sub}_task-arrow_run-02_events.tsv')
     a3 = pd.read_table(func_dir + f'sub-{sub}_task-arrow_run-03_events.tsv')
@@ -22,7 +34,7 @@ def main(data_dir, sub):
         arr_run.insert(3, 'object', 0, True)
         for index, row in arr_run.iterrows():
             arr_run.at[index, 'object'] = (row['triad'] - 1) * 3 + row['position']
-        arr_run = arr_run.fillna(0)  
+        arr_run = arr_run.dropna()
         out = (func_dir + f'sub-{sub}_task-arrow_run-0{run}_events.tsv')
         arr_run.to_csv(out, sep='\t', index=False)
         print('fixed run ' + str(run))
@@ -34,3 +46,4 @@ if __name__ == "__main__":
     parser.add_argument("sub", help="subject number e.g. temple001")
     args = parser.parse_args()
     main(args.data_dir, args.sub)
+
